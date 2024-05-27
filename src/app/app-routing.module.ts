8cae9709284ae97d3456e8cd02638e89252ae0c3
Nahum Josael Ramirez Lopez <148998254+MYXIEMPRESA@users.noreply.github.com>
2024-05-27 01:33:39 -0600

    mas mamadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,16 +5,22 @@ import { SignInComponent } from './sign-in/sign-in.component';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
 
 
 const routes: Routes = [ 
-  {path: 'singUp', component: SignUpComponent},
+  {path: 'signUp', component: SignUpComponent},
   {path: 'signIn', component: SignInComponent},
   { path: 'about', component: AboutComponent},
   { path: '', component: HomeComponent}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebase)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  apiKey: 'pk.eyJ1IjoibmFodW1sb3B6IiwiYSI6ImNsc3k2eGRlejBhYXIycm54YmxnaXlneTkifQ.wrnk_DcsIDYJ6a8OQSeE5Q',
+  firebase: {
+    projectId: 'triptroveangular',
+    appId: '1:424821813680:web:8e62cec178c0ffc4972675',
+    storageBucket: 'triptroveangular.appspot.com',
+    apiKey: 'AIzaSyA9r2Hn3Qoc7EaCkG0q9LVIGj_N6UluTZ4',
+    authDomain: 'triptroveangular.firebaseapp.com',
+    messagingSenderId: '424821813680',
+    measurementId: 'G-JB2EZRFDDX',
+  },
+};
